4a2086d7edd486bb03ae3ba503b4300a4242ebae
Lorenzo Battilocchi <lorenzo.battilocchi@ucdconnect.ie>
2019-05-21 16:01:57 +0200

    fix index.js typo

--- src/index.js
+++ src/index.js
@@ -4,48 +4,19 @@ import './index.css';
 
 
 
-//aaaa
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
+class Weather extends React.Component {
+  var unirest = require('unirest');
+
+  request()
+  {
+    unirest.get("http://community-open-weather-map.p.rapidapi.com/weather?callback=test&id=-2172797&units=metric&mode=html&q=Dunshaughlin")
+    .header("X-RapidAPI-Key","a86209baebmsh384b78fcb1d3c38p121c34jsnda3266b5473a")
+    .end(function (result) {
+      console.log(result.status, result.headers, result.body);
+     });
+
   }
 }
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
 class Game extends React.Component {
   render() {
     return (
