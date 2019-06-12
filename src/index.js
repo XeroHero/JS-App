ba26e27e8d47fa2ad49ed01c4999a5673233108f
Lorenzo Battilocchi <lorenzo.battilocchi@ucdconnect.ie>
2019-06-12 21:13:16 +0200

    Update GH Pages

--- src/index.js
+++ src/index.js
@@ -1,66 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-import { Link } from 'react-router-dom';
-import FacebookLogin from '../src/facebook';
-import FacebookLoginWithButton from '../src/facebook-with-button'
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const responseFacebook = (response) => {
-  console.log(response);
-};
+ReactDOM.render(<App />, document.getElementById('root'));
 
-class Base extends Component {
-  render() {
-    return (
-      <div>
-        <Link to="/dummy">Route to dummy page</Link>
-
-        <div>
-          <p>Facebook login with default button and styling</p>
-          <FacebookLoginWithButton
-            appId="490652501351020"
-            autoLoad
-            callback={responseFacebook}
-            icon="fa-facebook"
-          />
-        </div>
-
-        <div>
-          <p>Facebook login with render prop (and no styling provided out the box)</p>
-          <FacebookLogin
-            appId="490652501351020"
-            autoLoad
-            callback={responseFacebook}
-            render={renderProps => (
-              <button onClick={renderProps.onClick}>This is my custom FB button</button>
-            )}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-
-class Dummy extends Component {
-  render() {
-    return (
-      <div>
-        <Link to="/">Back</Link>
-        <h1>
-          This is just a dummy page to test the button<br />
-          <a href="https://fb.com/">
-            survives back and forth routing
-          </a>
-        </h1>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={Base}/>
-    <Route path="/dummy" component={Dummy}/>
-  </Router>,
-  document.getElementById('demo')
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
