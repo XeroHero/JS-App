4498db6a3a7e33541517d5929be6cd6d7a9f48f6
Lorenzo Battilocchi <lorenzo.battilocchi@ucdconnect.ie>
2019-06-20 15:58:06 +0100

    Update App ID to point to GetVolvd

--- src/components/facebook.js
+++ src/components/facebook.js
@@ -45,7 +45,7 @@ export default class Facebook extends Component {
     } else {
       fbContent = (
         <FacebookLogin
-          appId="189486938370592"
+          appId="490652501351020"
           autoLoad={true}
           fields="name,email,picture"
           onClick={this.componentClicked}
