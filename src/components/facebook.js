0298e57556df4bb981f248e0344bb1dfd4a954dc
Lorenzo Battilocchi <Lorenzo.battilocchi@ucdconnect.ie>
2019-06-25 18:59:54 +0100

    Remove profile view in web render

--- src/components/facebook.js
+++ src/components/facebook.js
@@ -27,33 +27,16 @@ export default class Facebook extends Component {
   render() {
     let fbContent;
 
-    if (this.state.isLoggedIn) {
-      fbContent = (
-        <div
-          style={{
-            width: "400px",
-            margin: "auto",
-            background: "#7adeac",
-            padding: "20px"
-          }}
-        >
-          <img src={this.state.picture} alt={this.state.name} />
-          <h2>Welcome {this.state.name}</h2>
-          Email: {this.state.email}
-        </div>
-      );
-    } else {
-      fbContent = (
-        <FacebookLogin
-          appId="490652501351020"
-          autoLoad={true}
-          autologoutlink="true"
-          fields="name,email,picture"
-          onClick={this.componentClicked}
-          callback={this.responseFacebook}
-        />
-      );
-    }
+    fbContent = (
+      <FacebookLogin
+        appId="490652501351020"
+        autoLoad={true}
+        autologoutlink="true"
+        fields="name,email,picture"
+        onClick={this.componentClicked}
+        callback={this.responseFacebook}
+      />
+    );
 
     return <div>{fbContent}</div>;
   }
