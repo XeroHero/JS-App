54bdf3c249082ab6f9b49f924edfa99d2b475e62
Lorenzo Battilocchi <Lorenzo.battilocchi@ucdconnect.ie>
2019-06-28 11:12:12 +0100

    Fix privacy policy

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
  For information on our use of your Facebook data click
         <a
           className="App-link"
-          href="static/privacy.html"
+          href="privacy.html"
           target="_blank"
           rel="noopener noreferrer"
           >
