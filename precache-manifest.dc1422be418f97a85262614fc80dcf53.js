3fcc4e3b8717eb9b04fcc1be9079730d36ce9076
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-06 18:42:41 +0000

    Deploy XeroHero/JS-App to github.com/XeroHero/JS-App.git:gh-pages

---
+++ precache-manifest.dc1422be418f97a85262614fc80dcf53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc99951fce24c845a3d7ca2726360cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "654d7448765a435f159c",
+    "url": "/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "4b620f26fd22eeee04ee",
+    "url": "/static/js/2.0b0b85fd.chunk.js"
+  },
+  {
+    "revision": "654d7448765a435f159c",
+    "url": "/static/js/main.3441a72c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
